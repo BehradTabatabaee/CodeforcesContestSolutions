15bcb2e4f32a8bbc9de3007f2349f1475526a762
Behrad Tabatabaee <behradtabatabaee3@gmail.com>
2024-07-30 12:22:04 +0330

    958A

---
+++ Codeforces Round 958 (Div. 2)/A.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <math.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <limits.h>
+#include <numeric>
+#include <chrono>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int ans = 0;
+        while (n > 1) {
+            ans++;
+            n = n - k + 1;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Solution to codeforces contest questions that i solve every day
 for now the order is:
 
 1. DIV 3 A-B (DONE)
-2. DIV 3 C
-3. DIV 2 A-B
+2. DIV 2 A-B
+3. DIV 3 C
 4. DIV 2 C
 5. DIV 3 D
