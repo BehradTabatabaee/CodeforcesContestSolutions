482bc19ea663f54faae95f0da5a1128486d99983
Behrad Tabatabaee <behradtabatabaee3@gmail.com>
2024-07-30 15:30:41 +0330

    956A

---
+++ Codeforces Round 956 (Div. 2)/A.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <math.h>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <limits.h>
+#include <numeric>
+#include <chrono>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> nums(n);
+        for (int i = 0; i < n; i++) {
+            nums[i] = i + 1;
+        }
+        for (auto& i : nums) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
